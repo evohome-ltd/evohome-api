6e6a07f638a3589a6b86127d7bfcc3481022cebf
Serhii Skveres <seager1802@gmail.com>
2024-06-09 11:31:56 +0300

    updated bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication, Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: INestApplication = await NestFactory.create(AppModule);
+
+  const logger: Logger = new Logger(bootstrap.name);
+
+  await app.listen(3000, '127.0.0.1', async () => {
+    logger.log(`EvoHome API running on ${await app.getUrl()}`);
+  });
 }
-bootstrap();
+
+(async () => {
+  await bootstrap();
+})();
