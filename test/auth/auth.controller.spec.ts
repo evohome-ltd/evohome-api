3eb55f58c44d215105294031192a6d56b162af2f
Serhii Skveres <seager1802@gmail.com>
2024-06-13 10:47:52 +0300

    fixed tests

--- test/auth/auth.controller.spec.ts
+++ test/auth/auth.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from '@app/auth/auth.controller';
+import { AuthService } from '@app/auth/auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +8,7 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [AuthService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- test/users/users.controller.spec.ts
+++ test/users/users.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from '@app/users/users.controller';
+import { UsersService } from '@app/users/users.service';
 
 describe('UserController', () => {
   let controller: UsersController;
@@ -7,6 +8,7 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [UsersService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
