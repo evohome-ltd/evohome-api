961cdbb2affbf085e655be1f70088d986a37f736
Serhii Skveres <seager1802@gmail.com>
2024-06-16 00:53:55 +0300

    fixed import

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { AuthService } from '@app/auth/auth.service';
 import { AccountModel } from '@app/database/models';
 import { AuthGuard } from '@app/auth/auth.guard';
